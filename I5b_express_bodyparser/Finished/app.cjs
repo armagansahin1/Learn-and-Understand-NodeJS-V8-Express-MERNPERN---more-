d46591193d09453f7a9cb6abc8e4d135d08ff4bb
armagan <armagan.sahin95@gmail.com>
2025-11-30 13:48:02 +0300

    NPM & Express

---
+++ I5b_express_bodyparser/Finished/app.cjs
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+const urlencodedParser = express.urlencoded({ extended: false });
+const jsonParser = express.json();
+
+app.use('/assets', express.static(__dirname + '/public'));
+
+app.set('view engine', 'ejs');
+
+app.use('/', function (req, res, next) {
+	console.log('Request Url:' + req.url);
+	next();
+});
+
+app.get('/', function(req, res) {
+	res.render('index');
+});
+
+app.get('/person/:id', function(req, res) {
+	res.render('person', { ID: req.params.id, Qstr: req.query.qstr });
+});
+
+app.post('/person', urlencodedParser, function(req, res) {
+	res.send('Thank you!');
+	console.log(req.body.firstname);
+	console.log(req.body.lastname);
+});
+
+app.post('/personjson', jsonParser, function(req, res) {
+	res.send('Thank you for the JSON data!');
+	console.log(req.body.firstname);
+	console.log(req.body.lastname);
+});
+
+app.get('/api', function(req, res) {
+	res.json({ firstname: 'John', lastname: 'Doe' });
+});
+
+app.listen(port);
